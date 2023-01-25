45ef18007e67ebe4e5906064980b1081944b936e
sdhex <studulstu@gmail.com>
2023-01-25 03:30:56 +0300

    submit form

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,31 @@
 const buttonEdit = document.querySelector('.profile__edit-button');
-const buttonClose = document.querySelector('.popup__close-button')
+const buttonClose = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 
-let profileTitle = document.querySelector('.profile__name').textContent;
-let profileDescription = document.querySelector('.profile__description').textContent;
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
 
-let popupTitle = document.querySelector('.popup__input_type_name');
+let popupName = document.querySelector('.popup__input_type_name');
 let popupDescription = document.querySelector('.popup__input_type_description');
+
+let popupForm = document.querySelector('.popup__form');
 
 buttonEdit.addEventListener('click', function () {
   popup.classList.add('popup_opened');
-  popupTitle.value = profileTitle;
-  popupDescription.value = profileDescription;
+  popupName.value = profileName.textContent;
+  popupDescription.value = profileDescription.textContent;
 });
 
 buttonClose.addEventListener('click', function () {
   popup.classList.remove('popup_opened');
 });
 
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  profileName.textContent = popupName.value;
+  profileDescription.textContent = popupDescription.value;
+  popup.classList.remove('popup_opened');
+}
+
+popupForm.addEventListener('submit', handleFormSubmit);
